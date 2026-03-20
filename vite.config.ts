b9daeae74cdc8059242ab801ef98f33eb76a7ba1
Sqap85 <enginelibol49@gmail.com>
2026-03-20 19:38:09 +0300

    feat: update favicon and manifest for improved PWA support
    - Replaced existing favicon with multiple formats (PNG, SVG, ICO) for better compatibility.
    - Added new apple-touch-icon and updated the web app manifest with appropriate icons.
    - Removed old calories.png references and updated Vite configuration to include new assets.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,12 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'calories.png'],
+      includeAssets: [
+        'favicon.ico',
+        'favicon.svg',
+        'favicon-96x96.png',
+        'apple-touch-icon.png',
+      ],
       manifest: {
         name: 'MacroMate',
         short_name: 'MacroMate',
@@ -22,12 +27,13 @@ export default defineConfig({
         prefer_related_applications: false,
         icons: [
           {
-            src: 'calories.png',
+            src: 'web-app-manifest-192x192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'any maskable',
           },
           {
-            src: 'calories.png',
+            src: 'web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
